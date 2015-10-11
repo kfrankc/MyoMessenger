c61a0497e2f50ab5e7ed2fb39b015dd36e080ad2
kluangkote <katiel97@hotmail.com>
2015-10-10 21:11:01 -0700

    Change ngrok url and add phone number

--- myo_myapp/scripts.js
+++ myo_myapp/scripts.js
@@ -2,16 +2,9 @@ var Myo = require('myo');
 var myMyo = Myo.create();
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var firstGesture = true;
-var numbers = ["+15624587737","+15598022442","+15105658237","+19512640510"];
+var numbers = ["+17143069521","+15624587737","+15598022442","+15105658237","+19512640510"];
 var number = "";
 myMyo.on('fist', function(edge){
-  myMyo.timer(edge, 500, function(){
-    console.log('Gesture: Fist');
-  })
-});
-
-myMyo.on('fingers_spread', function(edge){
-  //Edge is true if it's the start of the pose, false if it's the end of the pose
   myMyo.timer(edge, 500, function(){
     if(firstGesture == true)
     {
@@ -22,14 +15,14 @@ myMyo.on('fingers_spread', function(edge){
     {
       firstGesture = true;
       var req = new XMLHttpRequest();
-      var message = "Fingers Spread"
-      req.open('GET', 'http://ebb489b9.ngrok.io/?message=' + message + '&number=' + number, true);
+      var message = "Fist"
+      req.open('GET', 'http://f14c04eb.ngrok.io/?message=' + message + '&number=' + number, true);
       req.send(null);
     }
   })
 });
 
-myMyo.on('wave_in', function(edge){
+myMyo.on('fingers_spread', function(edge){
   //Edge is true if it's the start of the pose, false if it's the end of the pose
   myMyo.timer(edge, 500, function(){
     if(firstGesture == true)
@@ -41,14 +34,14 @@ myMyo.on('wave_in', function(edge){
     {
       firstGesture = true;
       var req = new XMLHttpRequest();
-      var message = "Wave In"
-      req.open('GET', 'http://ebb489b9.ngrok.io/?message=' + message + '&number=' + number, true);
+      var message = "Fingers Spread"
+      req.open('GET', 'http://f14c04eb.ngrok.io/?message=' + message + '&number=' + number, true);
       req.send(null);
     }
   })
 });
 
-myMyo.on('wave_out', function(edge){
+myMyo.on('wave_in', function(edge){
   //Edge is true if it's the start of the pose, false if it's the end of the pose
   myMyo.timer(edge, 500, function(){
     if(firstGesture == true)
@@ -60,14 +53,14 @@ myMyo.on('wave_out', function(edge){
     {
       firstGesture = true;
       var req = new XMLHttpRequest();
-      var message = "Wave Out"
-      req.open('GET', 'http://ebb489b9.ngrok.io/?message=' + message + '&number=' + number, true);
+      var message = "Wave In"
+      req.open('GET', 'http://f14c04eb.ngrok.io/?message=' + message + '&number=' + number, true);
       req.send(null);
     }
   })
 });
 
-myMyo.on('thumb_to_pinky', function(edge){
+myMyo.on('wave_out', function(edge){
   //Edge is true if it's the start of the pose, false if it's the end of the pose
   myMyo.timer(edge, 500, function(){
     if(firstGesture == true)
@@ -79,8 +72,27 @@ myMyo.on('thumb_to_pinky', function(edge){
     {
       firstGesture = true;
       var req = new XMLHttpRequest();
+      var message = "Wave Out"
+      req.open('GET', 'http://f14c04eb.ngrok.io/?message=' + message + '&number=' + number, true);
+      req.send(null);
+    }
+  })
+});
+
+myMyo.on('thumb_to_pinky', function(edge){
+  //Edge is true if it's the start of the pose, false if it's the end of the pose
+  myMyo.timer(edge, 500, function(){
+    if(firstGesture == true)
+    {
+      number = numbers[4];
+      firstGesture = false;
+    }
+    else
+    {
+      firstGesture = true;
+      var req = new XMLHttpRequest();
       var message = "Double Tap"
-      req.open('GET', 'http://ebb489b9.ngrok.io/?message=' + message + '&number=' + number, true);
+      req.open('GET', 'http://f14c04eb.ngrok.io/?message=' + message + '&number=' + number, true);
       req.send(null);
     }
   })
